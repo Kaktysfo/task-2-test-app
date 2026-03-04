2238728b6aa41dc44403724daac83e7b49de29d4
KAKTYS <muzzer.show@yandex.ru>
2026-03-04 20:26:15 +0300

    fixed app closing

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/Kaktysfo/app/calendar"
 	"github.com/Kaktysfo/app/events"
 )
 
 func main() {
-	e := events.Event{
-		Title:   "Встреча",
-		StartAt: time.Now(),
+	event1, err := events.NewEvent("День рождения", "2026/09/03 13:33")
+	if err != nil {
+		fmt.Println("Ошибка создания события:", err)
 	}
-	calendar.AddEvent("event1", e)
-	fmt.Println("Календарь обновлён")
+	event2, err := events.NewEvent("Уйти в запой", "03.09.2026")
+	calendar.AddEvent("event 1", event1)
+	calendar.AddEvent("event 2", event2)
+	calendar.ShowEvents()
 	fmt.Scanln()
 }
