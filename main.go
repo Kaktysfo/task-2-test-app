ed1303074ce1a911dc41878b8113ac2530ec83f8
KAKTYS <muzzer.show@yandex.ru>
2026-03-08 18:58:41 +0300

    + modified main func

--- main.go
+++ main.go
@@ -4,39 +4,33 @@ import (
 	"fmt"
 
 	"github.com/Kaktysfo/app/calendar"
-	"github.com/Kaktysfo/app/events"
 )
 
 func main() {
-	event1, err := events.NewEvent("День рождения", "2026/09/03 13:33")
-	if err != nil {
-		fmt.Println("Ошибка создания события:", err)
-	} else {
-		calendar.AddEvent("event 1", event1)
-	}
-	event2, err2 := events.NewEvent("Уйти в запой", "03/09/2026")
-	if err2 != nil {
-		fmt.Println("Ошибка создания события:", err2)
-	} else {
-		calendar.AddEvent("event 2", event2)
-	}
-	event3, err3 := events.NewEvent("popopooop", "03/09/2026 15:15")
-	if err3 != nil {
-		fmt.Println("Ошибка создания события:", err3)
-	} else {
-		calendar.AddEvent("event 3", event3)
-	}
-	err11 := calendar.DeleteEvent("event 3")
-	if err11 != nil {
-		fmt.Println(err11)
-	}
-	err12 := calendar.EditEvent("event 2", "jopa123321", "12/12/2026 11:15")
-	if err12 != nil {
-		fmt.Println(err12)
-	}
-	calendar.ShowEvents()
-	_, errScan := fmt.Scanln()
-	if errScan != nil {
+	event1, err1 := calendar.AddEvent("День рождения", "2026/09/03 13:33")
+	if err1 != nil {
+		fmt.Println("Ошибка:", err1)
 		return
+	}
+	event2, err2 := calendar.AddEvent("Уйти в запой", "03/09/2026")
+	if err2 != nil {
+		fmt.Println("Ошибка:", err2)
+		return
+	}
+	errShow := calendar.ShowEvents()
+	if errShow != nil {
+		fmt.Println(errShow)
+	}
+	errDel := calendar.DeleteEvent(event1.ID)
+	if errDel != nil {
+		fmt.Println(errDel)
+	}
+	err := calendar.EditEvent(event2.ID, "Созвон", "2025/06/12 16:50")
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+	}
+	errShow2 := calendar.ShowEvents()
+	if errShow2 != nil {
+		fmt.Println(errShow2)
 	}
 }
