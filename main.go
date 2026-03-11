e0b205ea75cc20b3a727f112c524000d4ffc1f52
KAKTYS <snegovik2312@gmail.com>
2026-03-11 17:43:06 +0300

    temp

--- main.go
+++ main.go
@@ -7,27 +7,25 @@ import (
 )
 
 func main() {
-	event1, err1 := calendar.AddEvent("День рождения", "2026/09/03 13:33")
+	c := calendar.NewCalendar()
+
+	event1, err1 := calendar.AddEvent("Meeting", "2025/06/12")
 	if err1 != nil {
-		fmt.Println("Ошибка:", err1)
-		return
+		fmt.Println("Error:", err1)
+	} else {
+		fmt.Println(event1.Title, "added")
 	}
-	event2, err2 := calendar.AddEvent("Уйти в запой", "03/09/2026")
+	event2, err2 := calendar.AddEvent("One more meeting", "2025/06/12")
 	if err2 != nil {
-		fmt.Println("Ошибка:", err2)
-		return
+		fmt.Println("Error:", err2)
+	} else {
+		fmt.Println(event2.Title, "added")
 	}
-	errShow := calendar.ShowEvents()
-	if errShow != nil {
-		fmt.Println(errShow)
-	}
-	errDel := calendar.DeleteEvent(event1.ID)
-	if errDel != nil {
-		fmt.Println(errDel)
-	}
-	err := calendar.EditEvent(event2.ID, "Созвон", "2025/06/12 16:50")
+	err := calendar.EditEvent(event2.ID, "Call", "2025/06/12 16:50")
 	if err != nil {
-		fmt.Println("Ошибка:", err)
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Event updated")
 	}
 	errShow2 := calendar.ShowEvents()
 	if errShow2 != nil {
